5a8cc641669f3765ec1dab365109c837f5a84d4b
mikan19 <mellamo.rina.ty@gmail.com>
2023-07-03 23:15:39 +0900

    fix:Blogアプリの[ログイン]機能をDDDの構造にしました。

--- src/public/user/signin.php
+++ src/public/user/signin.php
@@ -1,33 +1,45 @@
 <?php
 session_start();
+$errors = $_SESSION['errors'] ?? [];
+unset($_SESSION['errors']);
 
-// エラーメッセージがセッションに保存されている場合に表示
-if (isset($_SESSION["error_message"])) {
-    echo "<p class='error'>" . $_SESSION["error_message"] . "</p>";
-    unset($_SESSION["error_message"]); // エラーメッセージを表示した後はセッションから削除
-}
+$successRegistedMessage = $_SESSION['message'] ?? '';
+unset($_SESSION['message']);
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>sample</title>
-    </head>
-    <body>
-        <!-- 会員登録フォーム -->
-        <form action="signin_complete.php" method="post">
-            <h3>ログイン</h3>
-            <table>
-                <tr>
-                    <td><input name="email" placeholder="Email"></td>
-                </tr>
-                <tr>
-                    <td><input name="password" type="password" placeholder="Password"></td>
-                </tr>
-            </table>    
-            <input type="submit" value="ログイン" />
-            <p><a href="signup.php">アカウントを作る</a></p>
-        </form>
-    </body>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gray-200 w-full h-screen flex justify-center items-center">
+    <div class="w-96  bg-white pt-10 pb-10 rounded-xl">
+        <div class="w-60 m-auto text-center">
+            <h2 class="text-2xl mb-5">ログイン</h2>
+            <h3 class="mb-5 text-xl"><?php echo $successRegistedMessage; ?></h3>
+            <?php if (!empty($errors)): ?>
+                <?php foreach ($errors as $error): ?>
+                    <p class="text-red-600"><?php echo $error; ?></p>
+                <?php endforeach; ?>
+            <?php endif; ?>
+            <form class="px-4" action="./signin_complete.php" method="POST">
+                <p><input class="border-2 border-gray-300 mb-5 w-full" type=“text” name="email" type="email" required placeholder="Email" value="<?php if (
+                    isset($_SESSION['email'])
+                ) {
+                    echo $_SESSION['email'];
+                } ?>"></p>
+                <p><input class="border-2 border-gray-300 mb-5 w-full" type="password" placeholder="Password" name="password"></p>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mb-5 w-full" type="submit">ログイン</button>
+            </form>
+            <a class="text-blue-500" href="./signup.php">アカウントを作る</a>
+        </div>
+    </div>
+</body>
+
 </html>
--- src/public/user/signin_complete.php
+++ src/public/user/signin_complete.php
@@ -1,44 +1,34 @@
 <?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+use App\Infrastructure\Redirect\Redirect;
+use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\InputPassword;
+use App\UseCase\UseCaseInput\SignInInput;
+use App\UseCase\UseCaseInteractor\SignInInteractor;
+
 session_start();
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
+try {
     if (empty($email) || empty($password)) {
-        $_SESSION["error_message"] = "パスワードとメールアドレスを入力してください";
-        header("Location: signin.php");
-        exit;
-    } else {
-        $dbUserName = 'root';
-        $dbPassword = 'password';
-
-        try {
-            $pdo = new PDO("mysql:host=mysql;dbname=blog;charset=utf8", $dbUserName, $dbPassword);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $stmt = $pdo->prepare("SELECT id, password FROM user WHERE email = :email");
-            $stmt->bindParam(':email', $email);
-            $stmt->execute();
-
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($user && password_verify($password, $user["password"])) {
-                // パスワードの検証が成功した場合
-                $_SESSION["user_id"] = $user["id"]; // ユーザーIDをセッションに保存
-                header("Location: user/index.php");
-                exit;
-            } else {
-                $_SESSION["error_message"] = "メールアドレスまたはパスワードが正しくありません。";
-                header("Location: signin.php");
-                exit;
-            }
-        } catch (PDOException $e) {
-            die("データベースへの接続に失敗しました: " . $e->getMessage());
-        }
+        throw new Exception('パスワードとメールアドレスを入力してください');
     }
+
+    $userEmail = new Email($email);
+    $inputPassword = new InputPassword($password);
+    $useCaseInput = new SignInInput($userEmail, $inputPassword);
+    $useCase = new SignInInteractor($useCaseInput);
+    $useCaseOutput = $useCase->handler();
+
+    if (!$useCaseOutput->isSuccess()) {
+        throw new Exception($useCaseOutput->message());
+    }
+    Redirect::handler('../index.php');
+} catch (Exception $e) {
+    $_SESSION['errors'][] = $e->getMessage();
+    Redirect::handler('./signin.php');
 }
-
-
+?>
 
 
